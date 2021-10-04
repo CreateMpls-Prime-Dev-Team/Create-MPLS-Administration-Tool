950588fd843a8bee25417fb049dc1f02d4c08cfb
Alex Goldberg <alexandergold53+github@gmail.com>
2021-10-04 15:01:22 -0600

    got the barebones of student page built out

--- src/components/AdminView/AddStudent.jsx
+++ src/components/AdminView/AddStudent.jsx
@@ -1,175 +1,164 @@
-import react from 'react'
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import react from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import "./AddStudent.css";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 function AddStudent() {
-return(
-    <div>
-        <div class = "headerClass"><h1>Add Student</h1></div>
-        <div class = "headerClass"><h1>Edit Student</h1></div>
+  //Creating local states for drop down data storage
+  const [grade, setGrade] = React.useState("");
+  const [ethnicity, setEthnicity] = React.useState("");
+  const [gender, setGender] = React.useState("");
 
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
+  const handleGrade = (event) => {
+    setGrade(event.target.value);
+  };
+  const handleEthnicity = (event) => {
+    setEthnicity(event.target.value);
+  };
+  const handleGender = (event) => {
+    setGender(event.target.value);
+  };
+  return (
+    <div id="mainDiv">
+      <div id="headerOne">
+        <h1>Add Student</h1>
       </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
+
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {/* First Name */}
+        <TextField required id="outlined-required" label="First Name" />
+        {/* Last Name */}
+        <TextField required id="outlined-required" label="Last Name" />
+
+        {/* Grade Selection Drop-Down */}
+        <span id="dropdown">
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Grade</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={grade}
+              label="Grade"
+              onChange={handleGrade}
+            >
+              <MenuItem value={1}>1st Grade</MenuItem>
+              <MenuItem value={2}>2nd Grade</MenuItem>
+              <MenuItem value={3}>3rd Grade</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* Ethnicity Selection Drop-Down */}
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Ethnicity</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={ethnicity}
+              label="Ethnicity"
+              onChange={handleEthnicity}
+            >
+              <MenuItem value={"Somali"}>Somali</MenuItem>
+              <MenuItem value={"Hispanic"}>Hispanic</MenuItem>
+              <MenuItem value={"Caucasian"}>Caucasian</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* Gender Selection Drop-Down */}
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Gender</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={gender}
+              label="Gender"
+              onChange={handleGender}
+            >
+              <MenuItem value={"Male"}>Male</MenuItem>
+              <MenuItem value={"Female"}>Female</MenuItem>
+            </Select>
+          </FormControl>
+          <Button id="addBttn" variant="outlined" endIcon={<AddIcon />}>
+            Add Student
+          </Button>
+        </span>
+      </Box>
+
+      <div id="headerTwo">
+        <h1>Edit Student</h1>
       </div>
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          disabled
-          id="standard-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          variant="standard"
-        />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="standard"
-        />
-      </div>
-    </Box>
+
+      <h1>*DROPDOWN STUDENT SEARCH HERE*</h1>
+
+      {/* Update Grade */}
+      <span id="dropdownTwo">
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">Update Grade</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select2"
+            value={grade}
+            label="Grade"
+            onChange={handleGrade}
+          >
+            <MenuItem value={1}>1st Grade</MenuItem>
+            <MenuItem value={2}>2nd Grade</MenuItem>
+            <MenuItem value={3}>3rd Grade</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/* Update Ethnicity  */}
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">Update Ethnicity</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select2"
+            value={ethnicity}
+            label="Ethnicity"
+            onChange={handleEthnicity}
+          >
+            <MenuItem value={"Somali"}>Somali</MenuItem>
+            <MenuItem value={"Hispanic"}>Hispanic</MenuItem>
+            <MenuItem value={"Caucasian"}>Caucasian</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/* Update Gender */}
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">Update Gender</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select2"
+            value={gender}
+            label="Gender"
+            onChange={handleGender}
+          >
+            <MenuItem value={"Male"}>Male</MenuItem>
+            <MenuItem value={"Female"}>Female</MenuItem>
+          </Select>
+        </FormControl>
+        <Button id="addBttn" variant="outlined">
+          Update Student
+        </Button>
+        <Button id="addBttn" color='error' variant="outlined">
+          Delete Student
+        </Button>
+      </span>
     </div>
-)}
+  );
+}
 
 export default AddStudent;

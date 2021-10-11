2c002deb4300325cef364fb0d402d92aefa72d4e
Alex Goldberg <alexandergold53+github@gmail.com>
2021-10-11 11:45:08 -0600

    mo charts

--- src/components/AdminView/Dashboard.jsx
+++ src/components/AdminView/Dashboard.jsx
@@ -1,15 +1,16 @@
 import react from 'react'
 import Chart from 'chart.js/auto';
 import './Dashboard.css'
-import { Pie } from 'react-chartjs-2';
+import { Pie, Line, Bar } from 'react-chartjs-2';
+import { height } from '@mui/system';
 function Dashboard() {
 
     const ethnicitydata = {
-        labels: ['White', 'Hispanic', 'Latino', 'Somali', 'FFFFFF', '12345'],
+        labels: ['White', 'Hispanic', 'Latino', 'Somali'],
         datasets: [
           {
-            label: '# of Votes',
-            data: [10, 20, 30, 10, 10, 20],
+            label: 'Ethnicity Data',
+            data: [10, 20, 30, 10],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -35,7 +36,7 @@ function Dashboard() {
         labels: ['Male', 'Female'],
         datasets: [
           {
-            label: '# of Votes',
+            label: 'Gender Data',
             data: [10, 20],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
@@ -58,23 +59,99 @@ function Dashboard() {
         ],
       };
 
+      const enrollmentdata = {
+        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [
+          {
+            label: 'Student Enrollment by Month',
+            data: [12, 19, 3, 5, 2, 1000, 1200, 1400, 1234, 1412, 1242, 5231],
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+          },
+        ],
+      };
+      
+      const baroptions = {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      };
+
+      const locationdata = {
+        labels: ['Minneapolis', 'St. Paul', 'Suburbs', 'Anoka', 'Hopkins', 'Minnetonka'],
+        datasets: [
+          {
+            label: 'Learning Lab',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(255, 99, 132)',
+          },
+          {
+            label: 'Robot League',
+            data: [2, 3, 20, 5, 1, 4],
+            backgroundColor: 'rgb(54, 162, 235)',
+          },
+          {
+            label: 'Coding Class Bro',
+            data: [3, 10, 13, 15, 22, 30],
+            backgroundColor: 'rgb(75, 192, 192)',
+          },
+        ],
+      };
+      
+      const stackbaroptions = {
+        scales: {
+          yAxes: [
+            {
+              stacked: true,
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              stacked: true,
+            },
+          ],
+        },
+      };
+      
+
+
 return(  
 <div id = "flex-chart-container" >
-    <div class ="flex-child" style={{width: "20em"}}>
+    <div class ="flex-child" style={{width: "20em", height: "20em"}}>
     <h4>Ethnicity </h4>
     <Pie id = "ethnicityPie" data={ethnicitydata} />
     </div>
 
-    <div div class ="flex-child" style={{width: "20em"}}>
+    <div div class ="flex-child" style={{width: "20em", height: "20em"}}>
     <h4>Gender </h4>
-    <Pie id = "ethnicityPie" data={genderdata} />
+    <Pie id = "genderPie" data={genderdata} />
     </div>
+
+    <div div class ="flex-child" >
+    <h4>Student Enrollment </h4>
+    <Line id ="studentEnrollment" data={enrollmentdata} options={baroptions} style={{width: "20em", height: "100em"}} />
+    </div>
+
+    <div div class ="flex-child" >
+    <h4>Enrollment by Location </h4>
+    <Bar data={locationdata} options={stackbaroptions} />
+    </div>
+
 </div>
 );
 
 
-    // <p>Gender Pie</p>
-    // <p>Student Enrollment Line Chart</p>
+  
     // <p>Enrollment by Location Bar Chart</p>
 
 

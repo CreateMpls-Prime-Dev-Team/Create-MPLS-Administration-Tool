990d63e3f55445b0a56f574f772593c98029546f
Curtis Kettler <curtiskettler@gmail.com>
2021-10-15 14:29:30 -0500

    table of recent attendance added to teach portal

--- src/components/TeacherPortal/TeacherPortal.jsx
+++ src/components/TeacherPortal/TeacherPortal.jsx
@@ -21,14 +21,19 @@ function TeacherPortal() {
         dispatch({ type: 'SET_PROGRAM_OCCURRENCE', payload: { id, history } })
     }
 
+    const onEditAttendance = () => {
+        console.log('editing attendance!');
+    }
+
     return (
         <>
         <Container sx={{ width: 370 }}>
             <img src="design_a.png" width="400" height="250"/>
-            <Typography variant="h4" align="left" sx={{ marginLeft: 3}}>{user.first_name} {user.last_name}</Typography>
+            <Typography variant="h4" align="left" sx={{ marginLeft: 2, marginBottom: 2}}>{user.first_name} {user.last_name}</Typography>
         </Container>
         <center>
-            <Paper elevation={24} sx={{ width: 370 }}>
+            <Paper elevation={12} sx={{ width: 370 }}>
+            <Typography variant="h5" sx={{ marginBottom: -4, marginTop: 3 }}>MY PROGRAMS</Typography>
             <TableContainer sx={{ maxWidth: 350, marginBottom: 5, marginTop: 5 }}>
                 <Table aria-label="program list">
                     <TableHead sx={{ borderBottom: 3, borderColor: 'grey.500' }}>
@@ -47,6 +52,34 @@ function TeacherPortal() {
                                 <TableCell align="left" sx={{ fontSize: 18 }}>{prg.name}</TableCell>
                                 <TableCell align="left" sx={{ fontSize: 18 }}>{prg.location}</TableCell>
                                 <TableCell ><IconButton onClick={() => onAttendanceButton(prg.id)}><PlaylistAddCheckIcon/></IconButton></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </Paper>
+
+            <Paper elevation={12} sx={{ width: 370 }}>
+                <Typography variant="h5" sx={{ marginBottom: -4, marginTop: 2 }}>RECENT ATTENDANCE</Typography>
+                <TableContainer sx={{ maxWidth: 350, marginBottom: 5, marginTop: 5 }}>
+                <Table aria-label="recent attendance">
+                    <TableHead sx={{ borderBottom: 3, borderColor: 'grey.500' }}>
+                        <TableRow>
+                            <TableCell sx={{ fontSize: 20}}>DATE</TableCell>
+                            <TableCell sx={{ fontSize: 20}}>PROGRAM</TableCell>
+                            <TableCell align="center" sx={{ fontSize: 20 }}>LOCATION</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { Object.keys(programs).length > 0 && programs.map((prg) => (
+                            <TableRow
+                                key={prg.id}
+                                sx={{ '&:last-child td, &:last-child th': { borderBottom: 3, borderColor: 'grey.500' } }}
+                                onClick={onEditAttendance}
+                                >
+                                <TableCell align="left" sx={{ fontSize: 18 }}>{prg.date}</TableCell>
+                                <TableCell align="left" sx={{ fontSize: 18 }}>{prg.name}</TableCell>
+                                <TableCell align="left" sx={{ fontSize: 18 }}>{prg.location}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
